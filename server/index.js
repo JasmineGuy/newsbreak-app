5be95036c8907032ac47db46f628d93411297898
JasmineGuy <guyjasmine.c@gmail.com>
2021-10-05 13:38:36 -0400

    deploy

--- server/index.js
+++ server/index.js
@@ -1,17 +1,22 @@
-const express = require("express");
 const path = require("path");
+const express = require("express");
 const app = express();
+const publicPath = path.join(__dirname, "..", "public");
+const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 4000;
-
-app.use(express.static("public"));
-
-app.use(express.json());
-
+app.use(express.static(publicPath));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build", "index.html"));
+  res.sendFile(path.join(publicPath, "index.html"));
 });
 
+// app.use(express.static("public"));
+
+// app.use(express.json());
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../build", "index.html"));
+// });
+
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Server is up on ${port}!`);
 });
